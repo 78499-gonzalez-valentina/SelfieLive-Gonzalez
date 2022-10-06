e2936a2d9244a6940548a5a17c2d22084b8ca096
vvalugonzalez <valu322@gmail.com>
2022-10-06 17:50:24 -0300

    fetch

--- js/servicios.js
+++ js/servicios.js
@@ -1,5 +1,10 @@
 const inputFiltrar = document.getElementById("buscarServicio")
 const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+const URL = "../assets/bbdd/servicios.json"
+let services = []
+let contenidoHTML = ""
+
+
 let servicios = [
     {
         id:1,
@@ -46,6 +51,31 @@ let servicios = [
     
 ]
 
+
+
+const mostrarError = ()=> {
+    return `<div class="error">
+                <h2>¡Ups...!</h2>
+                <p>No pudimos cargar la información.</p>
+                <p>Por favor, intenta nuevamente en unos minutos.</p>
+            </div>`
+}
+
+const cargarContenido  = async ()=> {
+    try {
+        const response = await fetch(URL)
+        const data = await response.json()
+              services = data 
+              services.forEach(serv => contenidoHTML += cargarServicios(serv))
+    } 
+    catch (error) {
+        contenidoHTML += mostrarError()
+    }
+    finally {
+        container.innerHTML = contenidoHTML
+    }
+}
+
 function cargarServicios(array){
     let container = document.querySelector('#container');
     console.log('container: ', container);
@@ -65,8 +95,8 @@ function cargarServicios(array){
    
    
     }
-   
-cargarServicios(servicios)
+cargarContenido()
+// cargarServicios(servicios)
 
 function filtrarServicios() { 
     inputFiltrar.value = inputFiltrar.value.trim().toUpperCase()
@@ -95,7 +125,6 @@ inputFiltrar.addEventListener("keydown", filtrarServicios)
 
 const eventoEnBotones = () => 
 {
-    debugger
     let botones = document.querySelectorAll('.learn-more');
     for (const boton of botones) {
         boton.addEventListener('click', ()=> {agregarAlCarrito(boton.id), toastSwal("Servicio agregado con exito!", "top-end", "success")})
